b090bedc171979739592ffcb9c039089f00b02ee
ozzi- <oz@oz-web.com>
2018-12-20 11:46:57 +0100

    Create doOpen.sh

---
+++ doOpen.sh
@@ -0,0 +1,14 @@
+curl -H "Authentication: ********@********" -H "Content-Type: application/json" https://YOUR.URL.COM:8443 \
+--data '{
+  "id": "23c1dea4-f74d-4eec-a4ad-033f3b4212f7",
+  "version": 0,
+  "occurredAt": "2018-12-19T05:22:45.157Z",
+  "meta": {
+    "siteId": "****************"
+  },
+  "name": "componentIsOpen",
+  "payload": {
+    "componentId": "****************"
+  }
+}'
+echo ""
